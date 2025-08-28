25dcc13b9281335190c8bb7227744ae1fec48f8f
t1mato <tnln3rd@gmail.com>
2025-08-27 19:14:07 -0700

    Initial commit, set up project space

---
+++ backend/src/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import adminRoutes from './routes/admin.route.js';
+import songRoutes from './routes/song.route.js';
+import albumRoutes from './routes/album.route.js';
+import statRoutes from './routes/stat.route.js';
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+
+app.use(express.json()); // to parse req.body
+
+// Route for users
+app.use("/api/users", userRoutes);
+
+// Route for authentication
+app.use("/api/auth", authRoutes);
+
+// Route for admin
+app.use("/api/admin", adminRoutes);
+
+// Route for songs
+app.use("/api/songs", songRoutes);
+
+// Route for albums
+app.use("/api/albums", albumRoutes);
+
+// Route for stats
+app.use("/api/stats", statRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+    connectDB();
+})
